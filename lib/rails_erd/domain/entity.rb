34c01bfe9e91289a15713e660d96f0b22b883235
Eaden McKee <mail@eaden.net>
2025-10-23 15:58:17 +1300

    Update Entity to represent Ruby classes instead of ActiveRecord models

--- lib/rails_erd/domain/entity.rb
+++ lib/rails_erd/domain/entity.rb
@@ -1,46 +1,36 @@
 module RailsERD
   class Domain
-    # Entities represent your Active Record models. Entities may be connected
-    # to other entities.
+    # Entities represent Ruby classes in your application.
     class Entity
       class << self
-        def from_models(domain, models) # @private :nodoc:
-          (concrete_from_models(domain, models) + abstract_from_models(domain, models)).sort
-        end
-
-        private
-
-        def concrete_from_models(domain, models)
-          models.collect { |model| new(domain, model.name, model) }
-        end
-
-        def abstract_from_models(domain, models)
-          models.collect(&:reflect_on_all_associations).flatten.collect { |association|
-            association.options[:as].to_s.classify if association.options[:as]
-          }.flatten.compact.uniq.collect { |name| new(domain, name) }
+        def from_parsed_class(parsed, domain)
+          new(domain, parsed)
         end
       end
 
       extend Inspectable
-      inspection_attributes :model
+      inspection_attributes :name
 
       # The domain in which this entity resides.
       attr_reader :domain
 
-      # The Active Record model that this entity corresponds to.
-      attr_reader :model
+      # The parsed class data.
+      attr_reader :parsed_class
 
-      # The name of this entity. Equal to the class name of the corresponding
-      # model (for concrete entities) or given name (for abstract entities).
+      # The name of this entity (the class name).
       attr_reader :name
 
-      def initialize(domain, name, model = nil) # @private :nodoc:
-        @domain, @name, @model = domain, name, model
+      def initialize(domain, parsed) # @private :nodoc:
+        @domain = domain
+        @parsed_class = parsed
+        @name = parsed[:class_name]
       end
 
-      # Returns an array of attributes for this entity.
+      # Returns an array of attributes (methods) for this entity.
       def attributes
-        @attributes ||= generalized? ? [] : Attribute.from_model(domain, model)
+        @attributes ||= parsed_class[:public_methods].map do |method_data|
+          Attribute.new(self, method_data)
+        end
       end
 
       # Returns an array of all relationships that this entity has with other
@@ -49,51 +39,48 @@ module RailsERD
         domain.relationships_by_entity_name(name)
       end
 
-      # Returns +true+ if this entity has any relationships with other models,
+      # Returns +true+ if this entity has any relationships with other classes,
       # +false+ otherwise.
       def connected?
         relationships.any?
       end
 
-      # Returns +true+ if this entity has no relationships with any other models,
+      # Returns +true+ if this entity has no relationships with any other classes,
       # +false+ otherwise. Opposite of +connected?+.
       def disconnected?
         relationships.none?
       end
 
-      # Returns +true+ if this entity is a generalization, which does not
-      # correspond with a database table. Generalized entities are either
-      # models that are defined as +abstract_class+ or they are constructed
-      # from polymorphic interfaces. Any +has_one+ or +has_many+ association
-      # that defines a polymorphic interface with <tt>:as => :name</tt> will
-      # lead to a generalized entity to be created.
+      # Returns +true+ if this entity is a framework base class.
       def generalized?
-        !model or !!model.abstract_class?
+        # Mark framework base classes as generalized
+        %w[ApplicationRecord ApplicationController ApplicationMailer ApplicationJob
+           ActionController::Base ActiveRecord::Base ActionMailer::Base ActiveJob::Base
+           Object BasicObject].include?(name)
       end
 
-      # Returns +true+ if this entity descends from another entity, and is
-      # represented in the same table as its parent. In Rails this concept is
-      # referred to as single-table inheritance. In entity-relationship
-      # diagrams it is called specialization.
+      # Returns +true+ if this entity has a superclass.
       def specialized?
-        !!model and !model.descends_from_active_record?
+        !parsed_class[:superclass].nil?
       end
 
-      # Returns +true+ if this entity does not correspond directly with a
-      # database table (if and only if the entity is specialized or
-      # generalized).
+      # Returns +true+ if this is a framework class.
       def virtual?
-        generalized? or specialized?
+        generalized?
       end
       alias_method :abstract?, :virtual?
 
-      # Returns all child entities, if this is a generalized entity.
+      # Returns all child entities, if this entity has any.
       def children
         @children ||= domain.specializations_by_entity_name(name).map(&:specialized)
       end
 
       def namespace
         $1 if name.match(/(.*)::.*/)
+      end
+
+      def ==(other)
+        other.is_a?(Entity) && other.name == name
       end
 
       def to_s # @private :nodoc:
